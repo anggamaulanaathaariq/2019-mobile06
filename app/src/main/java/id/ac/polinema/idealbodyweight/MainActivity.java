f875891529e86e6c27c158a262df8155ec89381b
Angga Maulana Athaariq <am.athaariq@gmail.com>
2019-09-30 13:20:59 +0700

    Bundle Update

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
+
 public class MainActivity extends AppCompatActivity {
+	private AboutFragment aboutFragment;
 
 	// Deklarasikan atribut Fragment di sini
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		aboutFragment = AboutFragment.newInstance("Angga Maulana Athaariq");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
@@ -20,13 +23,18 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.menu, menu);
-		return  true;
+		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 		// TODO: Tambahkan penanganan menu di sini
-
+		if (item.getItemId() == R.id.menu_about) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container, aboutFragment)
+					.addToBackStack(null)
+					.commit();
+		}
 		return super.onOptionsItemSelected(item);
 	}
 }
